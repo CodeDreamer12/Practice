33ac3d065d313e975a0e2536c36a09a4a85f8db1
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-08-28 13:46:27 +0530

    Create Display 2-d Array.cpp

---
+++ Questions of Functions/Display 2-d Array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+const int n=2,m=2;
+using namespace std;
+void display(int arr[n][m]){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
+int main(){
+    int arr[n][m];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>arr[i][j];
+        }
+    }
+    display(arr);
+}
