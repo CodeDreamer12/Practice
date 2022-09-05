4e958a9b389d364ff85e692962180056d53cfea4
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-09-05 23:43:29 +0530

    Create basic2.cpp

---
+++ Destructor/basic2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class car{
+    private:
+    int model;
+    float cost;
+    string *color;
+    public:
+    car(int m1,float c1){
+        model=m1;
+        cost=c1;
+    }
+    car(){
+        color=new string;
+    }
+    void display();
+    car(car &t){
+        model=t.model;
+        cost=t.cost;
+    }
+    ~car(){
+        cout<<"Destructor"<<endl;
+    }
+};
+void car::display(){
+    cout<<model<<" "<<cost<<endl;
+}
+int main(){
+    car c1(2022,56.89);
+    car c2=c1;
+    c1.display();
+    c2.display();
+    return 0;
+}
