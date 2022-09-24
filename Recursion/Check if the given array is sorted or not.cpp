67a3a1ef84ddef6f714e8b18d34fe5bb3b70dccc
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-09-24 21:03:41 +0530

    Create Check if the given array is sorted or not.cpp

---
+++ Recursion/Check if the given array is sorted or not.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+bool sorted(int arr[], int n){
+    if(n==1){
+        return true;
+    }
+    bool rest_array=sorted(arr+1, n-1);
+    return(arr[0]<arr[1] && rest_array);
+}
+
+int main(){
+    int arr[]={1,18,3,4,5};
+    cout<<sorted(arr,5)<<endl;
+}
