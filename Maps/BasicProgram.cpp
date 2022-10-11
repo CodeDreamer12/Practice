fa78c211fc289070391a4dc8ebea140546df1d2d
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2022-10-11 22:08:17 +0530

    Create BasicProgram.cpp

---
+++ Maps/BasicProgram.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+    map<int,int> m;  //only declaring
+    
+    // m[1]=10;     //here 1 is key and 10 is value
+    // m[2]=20;
+    // m[3]=30;
+    
+    // map<char,int> m={{'a',1},{'b',2},{'c',3}}; //declaring & initializing at same time
+    // map<char,int>::iterator i;
+    
+    // m.insert({100,2});
+    // m.insert({20,10});
+    // m.insert({1,40});
+    // map<int,int>:: iterator i;
+    
+    
+    // for(i=m.begin(); i!=m.end(); i++){
+    //     cout<<(*i).first<<" "<<(*i).second<<endl;
+    // }
+    
+}
